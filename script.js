a6a25b0f0e80c69da2d393725dc4c2e61acbd6cb
Alonelytower <lonelytower2021@gmail.com>
2021-10-11 18:27:59 -0700

    Add basic gameplay loop.

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
+//              Rock    Paper   Scissors
+// Rock         Draw    Lose    Win
+// Paper        Win     Draw    Lose
+// Scissors     Lose    Win     Draw
+
+function game() {
+    let computerChoice = computerPlay();
+    let playerChoice = prompt("Enter your choice!");
+    playerChoice = playerChoice.toLowerCase();
+    if (computerChoice === 'rock' && playerChoice === 'paper') {
+        return "You Win! Paper beats Rock!";
+    } else if (computerChoice === 'rock' && playerChoice === 'scissors') {
+        return "You Lose! Rock beats Scissors!";
+    } else if (computerChoice === 'paper' && playerChoice === 'rock') {
+        return "You Lose! Paper beats Rock!";
+    } else if (computerChoice === 'paper' && playerChoice === 'scissors') {
+        return "You Win! Scissors beats Paper!";
+    } else if (computerChoice === 'scissors' && playerChoice === 'rock') {
+        return "You Win! Rock beats Scissors!";
+    } else if (computerChoice === 'scissors' && playerChoice === 'paper') {
+        return "You Lose! Scissors beats Paper!";
+    } else {
+        return `Draw! ${computerChoice} and ${playerChoice}`;
+    }
+}
+
 function computerPlay() {
     const NUM_CHOICES = 3;
     let number = _randomNumber(NUM_CHOICES);
@@ -23,4 +49,4 @@ function _randomNumber(ceiling) {
     return Math.floor(Math.random() * ceiling);
 }
 
-console.log(computerPlay());
+console.log(game());
